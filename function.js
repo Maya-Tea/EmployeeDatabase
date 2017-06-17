87f2c53cda454cea82212c8d8bc95eab5ffaddc1
Daniel Rietveld <DanielRietveld@Daniels-MacBook-Air.local>
2017-06-17 11:47:21 -0600

    no changes

---
+++ function.js
@@ -0,0 +1,40 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBuF6DtdPs0Iagr-_h9MZQ5ToBc1X64Jf4",
+    authDomain: "timesheet-27432.firebaseapp.com",
+    databaseURL: "https://timesheet-27432.firebaseio.com",
+    projectId: "timesheet-27432",
+    storageBucket: "timesheet-27432.appspot.com",
+    messagingSenderId: "576832095576"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+function writeToGrid(objectToWrite){
+	 database.ref().set(objectToWrite);
+	}
+
+$("#addButton").on('click', function(event){
+	event.preventDefault();
+
+var objectToUpdate = {}
+
+      objectToUpdate.name = $('#name-input').val().trim();
+      objectToUpdate.role = $('#role-input').val().trim();
+      objectToUpdate.start = $('#start-input').val().trim();
+      objectToUpdate.rate = $('#rate-input').val().trim();
+
+      console.log(objectToUpdate);
+
+      writeToGrid(objectToUpdate);
+
+database.ref().push({
+	name: objectToUpdate.name,
+	role: objectToUpdate.role,
+	start: objectToUpdate.start,
+	rate: objectToUpdate.rate
+});
+
+
+});
